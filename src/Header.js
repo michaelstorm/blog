08ebfbb665cb3a2593784c2bbc05527c527b30d7
Michael Storm <mstorm7@gmail.com>
2018-05-22 18:57:24 -0700

    WIP

--- src/Header.js
+++ src/Header.js
@@ -8,13 +8,13 @@ export default class Header extends Component {
           Michael Storm
         </a>
         <a href="https://github.com/michaelstorm" className="header--link">
-          <img src="file:///home/michael/blog/src/images/GitHub-Mark-32px.png" className="header--link" /> <span>GitHub</span>
+          <img src="src/images/GitHub-Mark-32px.png" className="header--link" /> <span>GitHub</span>
         </a>
         <a href="/resume" className="header--link">
           Resume
         </a>
         <a href="/rss" className="header--link">
-          <img src="file:///home/michael/blog/src/images/feed-icon-28x28.png" className="header--link" /> <span>RSS</span>
+          <img src="src/images/feed-icon-28x28.png" className="header--link" /> <span>RSS</span>
         </a>
       </div>
     );
